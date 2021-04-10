11be4647b901d376dcd78966d158fad0d0aa98a5
mnoah <noahvrazo@gmail.com>
2021-04-10 15:11:23 -0400

    4/10

---
+++ src/model/AddPart.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class AddPart {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
